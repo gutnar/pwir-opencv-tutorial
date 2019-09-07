6843937e1e6923182cac46ca811b9a7c3b35d7df
Gutnar Leede <gutnar@gmail.com>
2019-09-07 15:30:50 +0300

    Add drawing circle around the biggest ball

--- test.py
+++ test.py
@@ -18,9 +18,19 @@ cap = cv2.VideoCapture(0)
 while cap.isOpened():
     _, frame = cap.read()
 
+    ball_mask = utils.apply_color_mask(frame, ball_color)
+    basket_mask = utils.apply_color_mask(frame, basket_color)
+
+    contours, hierarchy = cv2.findContours(ball_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    circles = map(cv2.minEnclosingCircle, contours)
+    circles = sorted(circles, key = lambda circle: circle[1])
+
+    if len(circles):
+        biggest_circle = circles[-1]
+        (x, y), radius = biggest_circle
+        cv2.circle(frame, (int(x), int(y)), int(radius), utils.get_color_range_mean(ball_color), 5)
+
     cv2.imshow("frame", frame)
-    cv2.imshow("balls", utils.apply_color_mask(frame, ball_color))
-    cv2.imshow("baskets", utils.apply_color_mask(frame, basket_color))
 
     if cv2.waitKey(10) & 0xFF == ord("q"):
         break
--- utils.py
+++ utils.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np
 
 
+def get_color_range_mean(color_range):
+    return tuple([
+        (color_range[0][i] + color_range[1][i]) // 2
+        for i in range(2)
+    ])
+
+
 def apply_color_mask(src, color_range):
     # Convert to HSV and apply color mask
     hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
